918db96c7151e6995eb0c576d895bb4ff20464ac
qa-gary-parker <qa.gary.parker@gmail.com>
2025-02-21 17:52:48 +0000

    updated web dev code

--- tests/web.spec.ts
+++ tests/web.spec.ts
@@ -10,3 +10,5 @@ test('get started link', async ({ page }) => {
   await page.getByRole('link', { name: 'Get started' }).click();
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+
