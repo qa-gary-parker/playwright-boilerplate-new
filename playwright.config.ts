e0fc36634402c3cbb46ed92e002a8dee263d9d9b
qa-gary-parker <qa.gary.parker@gmail.com>
2025-02-22 18:31:04 +0000

    fix report

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,8 +23,10 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-    ['list'], // Default console output
-    ['blob']
+    ['list'],
+    ['blob'],
+    // Default console output
+    ['junit', { outputFile: 'test-results/results.xml' }] // JUnit XML for Jenkins
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
